3002fd7732225954e3a6009e5ffdd15dce8921b3
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-12-08 16:49:42 -0500

    Changes, including saving!

--- js/main.js
+++ js/main.js
@@ -77,7 +77,9 @@ function makeVisible(){
 function getAutoCalculators() {
 	calculators += cps/100;
 	totalCalculators.innerHTML = Math.floor(calculators) + " Calculators";
+	perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	makeVisible();
+	buttonChecker();
 }
 
 function getSeller() {
@@ -90,7 +92,6 @@ function getSeller() {
 		nextSeller *= 1.15;
 		button2.innerHTML = "Partner with a seller (+" + (sellerOutput * 100) + " cps)" + "<br>(" + Math.floor(nextSeller) + " Calculators)";
 		cps += sellerOutput * 100;
-		perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	}
 }
 
@@ -104,7 +105,6 @@ function befriendGeek() {
 		nextGeek *= 1.15;
 		button3.innerHTML = "Befriend a Geek (+" + (geekOutput * 100) + " cps)" + "<br>(" + Math.floor(nextGeek) + " Calculators)";
 		cps += geekOutput * 100;
-		perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	}
 }
 
@@ -118,7 +118,6 @@ function findWizard() {
 		nextWizard *= 1.15;
 		button4.innerHTML = "Find a Wizard (+" + (wizardOutput * 100) + " cps)" + "<br>(" + Math.floor(nextWizard) + " Calculators)";
 		cps += wizardOutput * 100;
-		perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	}
 }
     
@@ -132,7 +131,6 @@ function harnessGuruPower() {
 		nextGuru *= 1.15;
 		button5.innerHTML = "Harness Guru Power (+" + (guruOutput * 100) + " cps)" + "<br>(" + Math.floor(nextGuru) + " Calculators)";
 		cps += guruOutput * 100;
-		perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	}
 }
 
@@ -146,7 +144,6 @@ function discoverMartians() {
 		kermMartian *= 1.15;
 		button6.innerHTML = "Discover Martian Life (+" + (martianOutput * 100) + " cps)" + "<br>(" + Math.floor(kermMartian) + " Calculators)";
 		cps += martianOutput * 100;
-		perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
 	}
 }
   
@@ -166,4 +163,120 @@ function ultimateOverclock() {
 
 window.onload = function () {
 	setInterval('getAutoCalculators()', 10);
+	setInterval('save()',30000);
+	load();
 }
+
+function load() {
+	if (localStorage.getItem("a")) {
+	  //Format:
+	//variable = Number(localStorage.getItem("variableName"));
+
+	  calculators = Number(localStorage.getItem("calculators"));
+    sellers = Number(localStorage.getItem("sellers"));
+    geeks = Number(localStorage.getItem("geeks"));
+    wizards = Number(localStorage.getItem("wizards"));
+    gurus = Number(localStorage.getItem("gurus"));
+    martians = Number(localStorage.getItem("martians"));
+    overclocks = Number(localStorage.getItem("overclocks"));
+    nextSeller = Number(localStorage.getItem("nextSeller"));
+    nextGeek = Number(localStorage.getItem("nextGeek"));
+    nextWizard = Number(localStorage.getItem("nextWizard"));
+    nextGuru = Number(localStorage.getItem("nextGuru"));
+    kermMartian = Number(localStorage.getItem("kermMartian"));
+    nextOverclock = Number(localStorage.getItem("nextOverclock"));
+    nextUpgrade = Number(localStorage.getItem("nextUpgrade"));
+    cps = Number(localStorage.getItem("cps"));
+    sellerOutput = Number(localStorage.getItem("sellerOutput"));
+    geekOutput = Number(localStorage.getItem("geekOutput"));
+    wizardOutput = Number(localStorage.getItem("wizardOutput"));
+    guruOutput = Number(localStorage.getItem("guruOutput"));
+    martianOutput = Number(localStorage.getItem("martianOutput"));
+    overclockOutput = Number(localStorage.getItem("overclockOutput"));
+
+    if (sellers != 0) {
+			button2.style.visibility = 'visible';
+			button2.innerHTML = "Partner with a seller (+" + (sellerOutput * 100) + " cps)" + "<br>(" + Math.floor(nextSeller) + " Calculators)";
+			totalSellers.style.visibility = 'visible';
+			totalSellers.innerHTML = sellers + " Sellers";
+		}
+		if (geeks != 0) {
+			button3.style.visibility = 'visible';
+			button3.innerHTML = "Befriend a Geek (+" + (geekOutput * 100) + " cps)" + "<br>(" + Math.floor(nextGeek) + " Calculators)";
+			totalGeeks.style.visibility = 'visible';
+			totalGeeks.innerHTML = geeks + " Geeks";
+		}
+		if (wizards != 0) {
+			button4.style.visibility = 'visible';
+			button4.innerHTML = "Find a Wizard (+" + (wizardOutput * 100) + " cps)" + "<br>(" + Math.floor(nextWizard) + " Calculators)";
+			totalWizards.style.visibility = 'visible';
+			totalWizards.innerHTML = wizards + " Wizards";
+		}
+		if (gurus != 0){
+			button5.style.visibility = 'visible';
+			button5.innerHTML = "Harness Guru Power (+" + (guruOutput * 100) + " cps)" + "<br>(" + Math.floor(nextGuru) + " Calculators)";
+			totalGurus.style.visibility = 'visible';
+			totalGurus.innerHTML = gurus + " Gurus";
+		}
+		if (martians != 0){
+			button6.style.visibility = 'visible';
+			button6.innerHTML = "Discover Martian Life (+" + (martianOutput * 100) + " cps)" + "<br>(" + Math.floor(kermMartian) + " Calculators)";
+			totalMartians.style.visibility = 'visible';
+			totalMartians.innerHTML = martians + " Martians";
+		}
+		if (overclocks != 0){
+			button7.style.visibility = 'visible';
+			button7.innerHTML = "The Ultimate Overclocking. (+" + (overclockOutput * 100) + " cps)" + "<br>(" + Math.floor(nextOverclock) + " Calculators)";
+			totalOverclockers.style.visibility = 'visible';
+			totalOverclockers.innerHTML = overclocks + " Overclockers";
+		}
+		upgrade.innerHTML = "New feature at " + nextUpgrade + " calculators.";
+  }
+  else
+  {
+  	save();
+  }
+}
+
+function save() {
+		//dummy variable, for recall later. Checks if the site has been visited before, aka there's a save.
+		localStorage.setItem("a",1);
+		//Format:
+	//localStorage.setItem("variableName",variable)
+		localStorage.setItem("calculators",calculators);
+    localStorage.setItem("sellers",sellers);
+    localStorage.setItem("geeks",geeks);
+    localStorage.setItem("wizards",wizards);
+    localStorage.setItem("gurus",gurus);
+    localStorage.setItem("martians",martians);
+    localStorage.setItem("overclocks",overclocks);
+    localStorage.setItem("nextSeller",nextSeller);
+    localStorage.setItem("nextGeek",nextGeek);
+    localStorage.setItem("nextWizard",nextWizard);
+    localStorage.setItem("nextGuru",nextGuru);
+    localStorage.setItem("kermMartian",kermMartian);
+    localStorage.setItem("nextOverclock",nextOverclock);
+    localStorage.setItem("nextUpgrade",nextUpgrade);
+    localStorage.setItem("cps",cps);
+		localStorage.setItem("sellerOutput",sellerOutput);
+    localStorage.setItem("geekOutput",geekOutput);
+    localStorage.setItem("wizardOutput",wizardOutput);
+    localStorage.setItem("guruOutput",guruOutput);
+    localStorage.setItem("martianOutput",martianOutput);
+    localStorage.setItem("overclockOutput",overclockOutput);
+    var n1 = document.createElement("P");
+    n1.appendChild(document.createTextNode("Game Auto-Saved."));
+		document.getElementById("messages").appendChild(n1);
+		setTimeout(function() {document.getElementById('messages').children[0].remove()}, 5000);
+}
+
+function buttonChecker() {
+	button2.disabled = !(calculators >= nextSeller);
+	button3.disabled = !(calculators >= nextGeek);
+	button4.disabled = !(calculators >= nextWizard);
+	button5.disabled = !(calculators >= nextGuru);
+	button6.disabled = !(calculators >= kermMartian);
+	button7.disabled = !(calculators >= nextOverclock);
+}
+
+
