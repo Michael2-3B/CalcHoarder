31577784e9d120978163a9ecfdc59c2116bd3afc
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2017-12-09 09:56:31 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ function getAutoCalculators() {
 	calculators += cps/100;
 	totalCalculators.innerHTML = Math.floor(calculators) + " Calculators";
 	perSecond.innerHTML = "(" + cps.toFixed(1) + " calculators per second)";
-	window.document.title = calculators + " calcs | Calculator Hoarder"
+	window.document.title = Math.floor(calculators) + " calcs | Calculator Hoarder"
 	makeVisible();
 	buttonChecker();
 }
